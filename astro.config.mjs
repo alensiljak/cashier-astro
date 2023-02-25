dca1be679ab1ec04062e9b94bc7ede4c01991537
Alen Šiljak <dev@alensiljak.eu.org>
2023-02-25 21:46:42 +0100

    trying to add tailwind and daisy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import AstroPWA from '@vite-pwa/astro'
+import AstroPWA from '@vite-pwa/astro';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [AstroPWA()]
+  integrations: [AstroPWA(), tailwind()]
 });
